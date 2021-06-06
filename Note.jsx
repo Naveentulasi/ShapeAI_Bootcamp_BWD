0aaa47fd354635cb426b590ad7c32ba4d138d439
Naveentulasi <85443345+Naveentulasi@users.noreply.github.com>
2021-06-07 01:16:05 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Note(){
+  return (
+    <div className="note">
+      <h1>Changing Children's lives</h1>
+      <p>Orphans Organisation is the nonprofit
+         organization that helps suffering children
+          worldwide. We create children’s homes and 
+          programs in India, Our full-service
+           children’s homes in India provide impoverished 
+           children with healthy food, clothes, education, healthcare, spirtual
+           </p>
+    </div>
+  );
+}
+
+export default Note;
